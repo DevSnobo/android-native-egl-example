838b8ca8cde1fd3501314c339d6b61b9c2f938e0
DevSnobo <robldibobl@gmail.com>
2019-05-06 22:13:59 +0200

    copied code from gles3jni sample, works. changing shader creation to read from file somehow breaks, needs more debugging.

--- app/src/main/jni/renderer.cpp
+++ app/src/main/jni/renderer.cpp
@@ -17,7 +17,7 @@
 #include <cstdint>
 #include <unistd.h>
 #include <pthread.h>
-#include <android/native_window.h> // requires ndk r5 or newer
+#include <android/native_window.h>
 #include <EGL/egl.h>
 #include <GLES3/gl3.h>
 
@@ -27,9 +27,8 @@
 #include "logger.h"
 #include "renderer.h"
 
-#define LOG_TAG "EglSample"
 
-static GLfloat vertices[] = {
+/*static GLfloat vertices[] = {
         -0x10000, -0x10000, -0x10000, 0x00000, 0x00000, 0x00000, 0x10000,
         0x10000, -0x10000, -0x10000, 0x10000, 0x00000, 0x00000, 0x10000,
         0x10000, 0x10000, -0x10000, 0x10000, 0x10000, 0x00000, 0x10000,
@@ -47,7 +46,217 @@ GLushort indices[] = {
         3, 7, 4, 3, 4, 0,
         4, 7, 6, 4, 6, 5,
         3, 0, 1, 3, 1, 2
+};*/
+
+const Vertex QUAD[4] = {
+        // Square with diagonal < 2 so that it fits in a [-1 .. 1]^2 square
+        // regardless of rotation.
+        {{-0.7f, -0.7f}, {0x00, 0xFF, 0x00}},
+        {{ 0.7f, -0.7f}, {0x00, 0x00, 0xFF}},
+        {{-0.7f,  0.7f}, {0xFF, 0x00, 0x00}},
+        {{ 0.7f,  0.7f}, {0xFF, 0xFF, 0xFF}},
 };
+
+bool checkGlError(const char* funcName) {
+    GLint err = glGetError();
+    if (err != GL_NO_ERROR) {
+        ALOGE("GL error after %s(): 0x%08x\n", funcName, err);
+        return true;
+    }
+    return false;
+}
+
+GLuint createShader(GLenum shaderType, const char* src) {
+    GLuint shader = glCreateShader(shaderType);
+    if (!shader) {
+        checkGlError("glCreateShader");
+        return 0;
+    }
+    glShaderSource(shader, 1, &src, NULL);
+
+    GLint compiled = GL_FALSE;
+    glCompileShader(shader);
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+    if (!compiled) {
+        GLint infoLogLen = 0;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLogLen);
+        if (infoLogLen > 0) {
+            GLchar* infoLog = (GLchar*)malloc(infoLogLen);
+            if (infoLog) {
+                glGetShaderInfoLog(shader, infoLogLen, NULL, infoLog);
+                ALOGE("Could not compile %s shader:\n%s\n",
+                      shaderType == GL_VERTEX_SHADER ? "vertex" : "fragment",
+                      infoLog);
+                free(infoLog);
+            }
+        }
+        glDeleteShader(shader);
+        return 0;
+    }
+
+    return shader;
+}
+
+GLuint createProgram(const char* vtxSrc, const char* fragSrc) {
+    GLuint vtxShader = 0;
+    GLuint fragShader = 0;
+    GLuint program = 0;
+    GLint linked = GL_FALSE;
+
+    vtxShader = createShader(GL_VERTEX_SHADER, vtxSrc);
+    if (!vtxShader)
+        goto exit;
+
+    fragShader = createShader(GL_FRAGMENT_SHADER, fragSrc);
+    if (!fragShader)
+        goto exit;
+
+    program = glCreateProgram();
+    if (!program) {
+        checkGlError("glCreateProgram");
+        goto exit;
+    }
+    glAttachShader(program, vtxShader);
+    glAttachShader(program, fragShader);
+
+    glLinkProgram(program);
+    glGetProgramiv(program, GL_LINK_STATUS, &linked);
+    if (!linked) {
+        ALOGE("Could not link program");
+        GLint infoLogLen = 0;
+        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLogLen);
+        if (infoLogLen) {
+            GLchar* infoLog = (GLchar*)malloc(infoLogLen);
+            if (infoLog) {
+                glGetProgramInfoLog(program, infoLogLen, NULL, infoLog);
+                ALOGE("Could not link program:\n%s\n", infoLog);
+                free(infoLog);
+            }
+        }
+        glDeleteProgram(program);
+        program = 0;
+    }
+
+    exit:
+    glDeleteShader(vtxShader);
+    glDeleteShader(fragShader);
+    return program;
+}
+
+Renderer::Renderer()
+        :   mNumInstances(0),
+            mLastFrameNs(0)
+{
+    memset(mScale, 0, sizeof(mScale));
+    memset(mAngularVelocity, 0, sizeof(mAngularVelocity));
+    memset(mAngles, 0, sizeof(mAngles));
+}
+
+Renderer::~Renderer() {
+}
+
+void Renderer::resize(int w, int h) {
+    auto offsets = mapOffsetBuf();
+    calcSceneParams(w, h, offsets);
+    unmapOffsetBuf();
+
+    // Auto gives a signed int :-(
+    for (auto i = (unsigned)0; i < mNumInstances; i++) {
+        mAngles[i] = drand48() * TWO_PI;
+        mAngularVelocity[i] = MAX_ROT_SPEED * (2.0*drand48() - 1.0);
+    }
+
+    mLastFrameNs = 0;
+
+    glViewport(0, 0, w, h);
+}
+
+void Renderer::calcSceneParams(unsigned int w, unsigned int h,
+                               float* offsets) {
+    // number of cells along the larger screen dimension
+    const float NCELLS_MAJOR = MAX_INSTANCES_PER_SIDE;
+    // cell size in scene space
+    const float CELL_SIZE = 2.0f / NCELLS_MAJOR;
+
+    // Calculations are done in "landscape", i.e. assuming dim[0] >= dim[1].
+    // Only at the end are values put in the opposite order if h > w.
+    const float dim[2] = {fmaxf(w,h), fminf(w,h)};
+    const float aspect[2] = {dim[0] / dim[1], dim[1] / dim[0]};
+    const float scene2clip[2] = {1.0f, aspect[0]};
+    const int ncells[2] = {
+            static_cast<int>(NCELLS_MAJOR),
+            (int)floorf(NCELLS_MAJOR * aspect[1])
+    };
+
+    float centers[2][MAX_INSTANCES_PER_SIDE];
+    for (int d = 0; d < 2; d++) {
+        auto offset = -ncells[d] / NCELLS_MAJOR; // -1.0 for d=0
+        for (auto i = 0; i < ncells[d]; i++) {
+            centers[d][i] = scene2clip[d] * (CELL_SIZE*(i + 0.5f) + offset);
+        }
+    }
+
+    int major = w >= h ? 0 : 1;
+    int minor = w >= h ? 1 : 0;
+    // outer product of centers[0] and centers[1]
+    for (int i = 0; i < ncells[0]; i++) {
+        for (int j = 0; j < ncells[1]; j++) {
+            int idx = i*ncells[1] + j;
+            offsets[2*idx + major] = centers[0][i];
+            offsets[2*idx + minor] = centers[1][j];
+        }
+    }
+
+    mNumInstances = ncells[0] * ncells[1];
+    mScale[major] = 0.5f * CELL_SIZE * scene2clip[0];
+    mScale[minor] = 0.5f * CELL_SIZE * scene2clip[1];
+}
+
+void Renderer::step() {
+    timespec now;
+    clock_gettime(CLOCK_MONOTONIC, &now);
+    auto nowNs = now.tv_sec*1000000000ull + now.tv_nsec;
+
+    if (mLastFrameNs > 0) {
+        float dt = float(nowNs - mLastFrameNs) * 0.000000001f;
+
+        for (unsigned int i = 0; i < mNumInstances; i++) {
+            mAngles[i] += mAngularVelocity[i] * dt;
+            if (mAngles[i] >= TWO_PI) {
+                mAngles[i] -= TWO_PI;
+            } else if (mAngles[i] <= -TWO_PI) {
+                mAngles[i] += TWO_PI;
+            }
+        }
+
+        float* transforms = mapTransformBuf();
+        for (unsigned int i = 0; i < mNumInstances; i++) {
+            float s = sinf(mAngles[i]);
+            float c = cosf(mAngles[i]);
+            transforms[4*i + 0] =  c * mScale[0];
+            transforms[4*i + 1] =  s * mScale[1];
+            transforms[4*i + 2] = -s * mScale[0];
+            transforms[4*i + 3] =  c * mScale[1];
+        }
+        unmapTransformBuf();
+    }
+
+    mLastFrameNs = nowNs;
+}
+
+void Renderer::render() {
+    step();
+
+    glClearColor(0.2f, 0.2f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    draw(mNumInstances);
+    checkGlError("Renderer::render");
+}
+
+
+
+
+/*
 
 Renderer::Renderer()
         : _msg(MSG_NONE), _display(nullptr), _surface(nullptr), _context(nullptr), _shader(nullptr),
@@ -75,15 +284,6 @@ void Renderer::stop() {
     pthread_join(_threadId, nullptr);
     LOG_INFO("Renderer thread stopped");
 }
-
-void Renderer::setWindow(ANativeWindow *window) {
-    // notify render thread that window has changed
-    pthread_mutex_lock(&_mutex);
-    _msg = MSG_WINDOW_SET;
-    _window = window;
-    pthread_mutex_unlock(&_mutex);
-}
-
 
 void Renderer::renderLoop() {
     bool renderingEnabled = true;
@@ -120,9 +320,13 @@ void Renderer::renderLoop() {
             if (!eglSwapBuffers(_display, _surface)) {
                 LOG_ERROR("eglSwapBuffers() returned error %d", eglGetError());
             }
+        } else {
+            LOG_ERROR("----- NOT DRAWING -----");
         }
 
         pthread_mutex_unlock(&_mutex);
+        //eglSwapBuffers(_display, _surface);
+        LOG_ERROR("----- NOT DRAWING -----");
     }
 
     LOG_INFO("Render loop exits");
@@ -219,7 +423,8 @@ bool Renderer::initialize() {
     _cube->vbuf->SetColorsOffset(3 * sizeof(GLfloat));
 
 
-    /*glGenVertexArrays(1, &VAO);
+    */
+/*glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
 
@@ -235,18 +440,23 @@ bool Renderer::initialize() {
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (GLvoid *) 0);
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat),
-                          (GLvoid *) (3 * sizeof(GLfloat)));*/
+                          (GLvoid *) (3 * sizeof(GLfloat)));*//*
+
 
     ratio = (GLfloat) width / height;
 
-    /* glm::vec4 vec = glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
+    */
+/* glm::vec4 vec = glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
      glm::mat4 trans = glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 1.0f, 0.0f));
      vec = trans * vec;
-     glm::mat4 viewMat = glm::lookAt(glm::vec3(0,0,10), glm::vec3(0,0,9), glm::vec3(-sin(0.1f), 0, cos(-0.f)));*/
+     glm::mat4 viewMat = glm::lookAt(glm::vec3(0,0,10), glm::vec3(0,0,9), glm::vec3(-sin(0.1f), 0, cos(-0.f)));*//*
 
-    /*glMatrixMode(GL_PROJECTION);
+
+    */
+/*glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glFrustumf(-ratio, ratio, -1, 1, 1, 10);*/
+    glFrustumf(-ratio, ratio, -1, 1, 1, 10);*//*
+
 
     return true;
 }
@@ -275,7 +485,8 @@ void Renderer::drawFrame() {
     _shader.Render(_cube->ibuf, &res);
 
 
-    /*glMatrixMode(GL_MODELVIEW);
+    */
+/*glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     glTranslatef(0, 0, -3.0f);
     glRotatef(_angle, 0, 1, 0);
@@ -289,7 +500,8 @@ void Renderer::drawFrame() {
     glColorPointer(4, GL_FIXED, 0, colors);
 
     glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, indices);
-    */
+    *//*
+
 
     _shader.EndRender();
     _angle += 1.2f;
@@ -303,3 +515,4 @@ void *Renderer::threadStartCallback(void *myself) {
     pthread_exit(nullptr);
 }
 
+*/
--- app/src/main/jni/renderer.h
+++ app/src/main/jni/renderer.h
@@ -23,20 +23,91 @@
 #include "rendering/shader.hpp"
 #include "rendering/simplegeom.hpp"
 
+// Include the latest possible header file( GL version header )
+#if __ANDROID_API__ >= 24
+#include <GLES3/gl32.h>
+#elif __ANDROID_API__ >= 21
+#include <GLES3/gl31.h>
+#else
+#include <GLES3/gl3.h>
+#endif
+
+#define DEBUG 1
+
+#define LOG_TAG "DemoRenderer"
+#define ALOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#if DEBUG
+#define ALOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
+#else
+#define ALOGV(...)
+#endif
+
+#define MAX_INSTANCES_PER_SIDE 16
+#define MAX_INSTANCES   (MAX_INSTANCES_PER_SIDE * MAX_INSTANCES_PER_SIDE)
+#define TWO_PI          (2.0 * M_PI)
+#define MAX_ROT_SPEED   (0.3 * TWO_PI)
+
+
+
+struct Vertex {
+    GLfloat pos[2];
+    GLubyte rgba[4];
+};
+extern const Vertex QUAD[4];
+extern bool checkGlError(const char* funcName);
+extern GLuint createShader(GLenum shaderType, const char* src);
+extern GLuint createProgram(const char* vtxSrc, const char* fragSrc);
 
 class Renderer {
+public:
+    virtual ~Renderer();
+    void resize(int w, int h);
+    void render();
+
+protected:
+    Renderer();
+
+    // return a pointer to a buffer of MAX_INSTANCES * sizeof(vec2).
+    // the buffer is filled with per-instance offsets, then unmapped.
+    virtual float* mapOffsetBuf() = 0;
+    virtual void unmapOffsetBuf() = 0;
+    // return a pointer to a buffer of MAX_INSTANCES * sizeof(vec4).
+    // the buffer is filled with per-instance scale and rotation transforms.
+    virtual float* mapTransformBuf() = 0;
+    virtual void unmapTransformBuf() = 0;
+
+    virtual void draw(unsigned int numInstances) = 0;
+
+private:
+    void calcSceneParams(unsigned int w, unsigned int h, float* offsets);
+    void step();
+
+    unsigned int mNumInstances;
+    float mScale[2];
+    float mAngularVelocity[MAX_INSTANCES];
+    uint64_t mLastFrameNs;
+    float mAngles[MAX_INSTANCES];
+};
+
+
+/*class Renderer {
 
 public:
     Renderer();
+
     virtual ~Renderer();
 
     // Following methods can be called from any thread.
     // They send message to render thread which executes required actions.
     void start();
+
     void stop();
-    void setWindow(ANativeWindow* window);
-    
-    
+
+    void setWindow(ANativeWindow *window);
+    void resize(int w, int h);
+    void render();
+
+
 private:
 
     enum RenderThreadMessage {
@@ -48,9 +119,9 @@ private:
     pthread_t _threadId{};
     pthread_mutex_t _mutex;
     enum RenderThreadMessage _msg;
-    
+
     // android window, supported by NDK r5 and newer
-    ANativeWindow* _window{};
+    ANativeWindow *_window{};
 
     EGLDisplay _display;
     EGLSurface _surface;
@@ -58,19 +129,23 @@ private:
     Shader _shader;
     SimpleGeom *_cube;
     GLfloat _angle;
-    
+
     // RenderLoop is called in a rendering thread started in start() method
     // It creates rendering context and renders scene until stop() is called
     void renderLoop();
-    
+
     bool initialize();
+
     void destroy();
 
     void drawFrame();
 
     // Helper method for starting the thread 
-    static void* threadStartCallback(void *myself);
+    static void *threadStartCallback(void *myself);
 
-};
+};*/
+
+extern Renderer* createES2Renderer();
+extern Renderer* createES3Renderer();
 
 #endif // RENDERER_H
